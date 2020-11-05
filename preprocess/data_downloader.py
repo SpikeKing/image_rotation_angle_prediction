fad1d562718149ca302162d8c798e41c3f951799
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-11-05 14:45:06 +0800

    增加数据预处理逻辑

---
+++ preprocess/data_downloader.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+"""
+Copyright (c) 2020. All rights reserved.
+Created by C. L. Wang on 4.11.20
+"""
+
+import json
+import os
+import sys
+from multiprocessing.pool import Pool
+
+import cv2
+import pandas as pd
+
+p = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if p not in sys.path:
+    sys.path.append(p)
+
+from myutils.project_utils import mkdir_if_not_exist, download_url_img, traverse_dir_files
+from root_dir import DATA_DIR
+
+
+class DataDownloader(object):
+    """
+    数据下载
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_boxes(boxes_str):
+        """
+        解析数据, 获取boxes列表
+        """
+        bbox_list = []
+        box_list = json.loads(boxes_str)
+        boxes = box_list[0]
+
+        n_box = len(boxes)
+        if n_box == 0:
+            return False, bbox_list
+
+        # 示例: {"coord":[54,21,1742,513]}
+        for box_dict in boxes:
+            bbox = box_dict["coord"]
+            bbox_list.append(bbox)
+
+        return True, bbox_list
+
+    @staticmethod
+    def output_img(out_dir, maker, idx, label_str, url):
+        """
+        根据是否包含box写入文件
+        """
+        is_box, box_list = DataDownloader.get_boxes(label_str)  # 是否写入box
+
+        if not is_box:
+            return 0
+
+        is_img, img_bgr = download_url_img(url)  # 下载图像
+        if not is_img:
+            return 0
+
+        out_name = "{}_{}_{}.jpg".format(idx, maker, len(box_list))
+        out_path = os.path.join(out_dir, out_name)
+
+        cv2.imwrite(out_path, img_bgr)
+        print('[Info] 写入文件: {}'.format(out_path))
+
+    def process_excel(self, data_path, out_dir, pool):
+        """
+        处理excel文件
+        """
+        # 标签: ["index", "markTime", "checkLabel", "label", "checker", "url", "marker", "_id", "taskId"]
+        pd_data = pd.read_csv(
+            data_path,
+            header=0,
+            names=["index", "markTime", "checkLabel", "label", "checker", "url", "marker", "_id", "taskId"]
+        )
+
+        labels = pd_data["label"]
+        urls = pd_data["url"]
+        indexes = pd_data["index"]
+        makers = pd_data["marker"]
+
+        for idx, (p_idx, maker, label_str, url) in enumerate(zip(indexes, makers, labels, urls)):
+            # 多进程存储图像
+            pool.apply_async(DataDownloader.output_img, args=(out_dir, p_idx, maker, label_str, url, True))
+            if idx % 1000 == 0:
+                print('[Info] idx: {}'.format(idx))
+
+        print('[Info] 下载完成: {}'.format(data_path))
+
+    def process_folder(self, task_folder, out_dir):
+        """
+        处理文件夹
+        """
+        mkdir_if_not_exist(out_dir)
+
+        paths_list, names_list = traverse_dir_files(task_folder)
+        n_prc = 40
+        pool = Pool(processes=n_prc)  # 多线程下载
+
+        for path, name in zip(paths_list, names_list):
+            self.process_excel(path, out_dir, pool)  # 处理excel文件
+
+        # 多进程逻辑
+        pool.close()
+        pool.join()
+
+        print('[Info] 全部下载完成')
+
+
+def main():
+    task_folder = os.path.join(DATA_DIR, 'task_files')
+    out_dir = os.path.join(DATA_DIR, 'task-raw-out')
+    dd = DataDownloader()
+    dd.process_folder(task_folder, out_dir)
+
+
+if __name__ == '__main__':
+    main()
--- preprocess/data_processor.py
+++ preprocess/data_processor.py
@@ -4,3 +4,110 @@
 Copyright (c) 2020. All rights reserved.
 Created by C. L. Wang on 4.11.20
 """
+
+import os
+import copy
+import cv2
+import numpy as np
+
+from myutils.cv_utils import get_patch
+from root_dir import DATA_DIR
+
+
+class DataProcessor(object):
+    """
+    数据处理
+    """
+    def __init__(self):
+        pass
+
+    def cut_img_without_margin(self, img_bgr):
+        """
+        去掉旋转图像四个角的黑色区域，保留最大的内接矩形
+        :param img_bgr: bgr图像
+        :return: 最大的内接矩形图像
+        """
+        from myutils.cv_utils import show_img_bgr
+        show_img_bgr(img_bgr)
+
+        h, w, _ = img_bgr.shape
+        img_copy = copy.copy(img_bgr)
+
+        # 从4个角，洪水漫灌，选择4个角的联通区域，
+        mask = np.zeros((h + 2, w + 2), np.uint8)  # Mask
+        cv2.floodFill(img_copy, mask=mask, seedPoint=(0, 0), newVal=(0, 0, 255),
+                      loDiff=(5, 5, 5, 5), upDiff=(5, 5, 5, 5))
+        cv2.floodFill(img_copy, mask=mask, seedPoint=(w-1, 0), newVal=(0, 0, 255),
+                      loDiff=(5, 5, 5, 5), upDiff=(5, 5, 5, 5))
+        cv2.floodFill(img_copy, mask=mask, seedPoint=(0, h-1), newVal=(0, 0, 255),
+                      loDiff=(5, 5, 5, 5), upDiff=(5, 5, 5, 5))
+        cv2.floodFill(img_copy, mask=mask, seedPoint=(w-1, h-1), newVal=(0, 0, 255),
+                      loDiff=(5, 5, 5, 5), upDiff=(5, 5, 5, 5))
+        show_img_bgr(img_copy)
+
+        mask_inv = np.where(mask > 0.5, 0, 1).astype(np.uint8)  # 反转mask的0和1
+
+        # 形态学Opening，过滤较小的非连通区域
+        kernel = np.ones((9, 9), np.uint8)
+        mask_inv = cv2.morphologyEx(mask_inv, cv2.MORPH_OPEN, kernel)
+        show_img_bgr(mask_inv * 255)
+
+        # 中心区域是1，其他位置是0
+        contours, hierarchy = cv2.findContours(mask_inv, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        c_points = np.squeeze(contours)  # 确保1个连通区域，多个连通区域会异常
+
+        # 测试连通区域
+        cv2.drawContours(img_bgr, contours, -1, (0, 255, 0), 3)
+        show_img_bgr(img_bgr)
+
+        # 获取点的全部X\Y坐标
+        x_list, y_list = [], []
+        for p in c_points:
+            x_list.append(p[0])
+            y_list.append(p[1])
+        x_list = np.asarray(x_list)
+        y_list = np.asarray(y_list)
+
+        # 从XY坐标中，提取区域的最大矩形区域，即[x_min, y_min, x_max, y_max]
+        x_min_idx = np.argmin(x_list)
+        y_1 = y_list[x_min_idx]
+        x_max_idx = np.argmax(x_list)
+        y_2 = y_list[x_max_idx]
+        y_max, y_min = max(y_1, y_2), min(y_1, y_2)
+
+        y_min_idx = np.argmin(y_list)
+        x_1 = x_list[y_min_idx]
+        y_max_idx = np.argmax(y_list)
+        x_2 = x_list[y_max_idx]
+        x_max, x_min = max(x_1, x_2), min(x_1, x_2)
+
+        # 获取核心的中心区域
+        img_patch = get_patch(img_bgr, [x_min, y_min, x_max, y_max])
+        show_img_bgr(img_patch)
+
+        # 写入输出图像
+        img_out_path = os.path.join(DATA_DIR, 'out1.jpg')
+        cv2.imwrite(img_out_path, img_patch)
+
+        print('[Info] 处理完成!')
+
+        return img_patch
+
+
+def demo_of_cut_img_without_margin():
+    """
+    测试cut_img_without_margin
+    """
+    dp = DataProcessor()
+
+    img_path = os.path.join(DATA_DIR, '1.jpg')
+    img_bgr = cv2.imread(img_path)
+    dp.cut_img_without_margin(img_bgr)
+
+
+def main():
+    demo_of_cut_img_without_margin()
+
+
+if __name__ == '__main__':
+    main()
