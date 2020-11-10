471f94ffe95555047e8a70ef6e0217e1f6774c9d
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-11-11 07:57:27 +0800

    update

--- train/train_problems.py
+++ train/train_problems.py
@@ -51,7 +51,7 @@ model.compile(loss='categorical_crossentropy',
 
 # training parameters
 batch_size = 128
-nb_epoch = 50
+nb_epoch = 200
 
 output_folder = 'models'
 if not os.path.exists(output_folder):
