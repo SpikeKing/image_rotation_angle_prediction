a60e5ef04106d24d49d0b6d39f796ca99b52f2fb
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-11-11 19:59:05 +0800

    update

--- train/train_problems.py
+++ train/train_problems.py
@@ -58,7 +58,7 @@ if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
 # 加载已有模型
-model.load_weights(os.path.join(output_folder, 'problem_rotnet_resnet50_4.9835.hdf5'))
+model.load_weights(os.path.join(output_folder, 'problem_rotnet_resnet50_4.6894.hdf5'))
 
 # callbacks
 monitor = 'val_angle_error'
