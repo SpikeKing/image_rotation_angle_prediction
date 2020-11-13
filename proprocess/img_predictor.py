cf3f47e3631e99a69f2ddfab8af948853d4a8dc8
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-11-13 20:43:17 +0800

    update

--- proprocess/img_predictor.py
+++ proprocess/img_predictor.py
@@ -34,6 +34,7 @@ class ImgPredictor(object):
         加载模型
         """
         model_location = os.path.join(DATA_DIR, 'models', 'problem_rotnet_resnet50.1.6377.hdf5')
+        print('[Info] model_location: {}'.format(model_location))
         model = load_model(model_location, custom_objects={'angle_error': angle_error})
         return model
 
