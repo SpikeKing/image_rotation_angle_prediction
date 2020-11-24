eef486128beb892cacc3a2d2658de40eb086ae21
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-11-24 21:12:17 +0800

    update

--- train/train_problems.py
+++ train/train_problems.py
@@ -25,33 +25,29 @@ from root_dir import ROOT_DIR, DATA_DIR
 from myutils.project_utils import get_current_time_str
 
 
-data1_path = os.path.join(ROOT_DIR, '..', 'datasets', 'rotation_datasets_12w')
+data1_path = os.path.join(ROOT_DIR, '..', 'datasets', 'datasets_v1_123974')
 print('[Info] data1_path: {}'.format(data1_path))
 train1_filenames, test1_filenames = get_problems_data(data1_path)
 train1_filenames = (train1_filenames + test1_filenames)
 random.shuffle(train1_filenames)
 print('[Info] data1 train: {}, test: {}'.format(len(train1_filenames), len(test1_filenames)))
 
-data2_path = os.path.join(ROOT_DIR, '..', 'datasets', 'biaozhu_csv_out')
+data2_path = os.path.join(ROOT_DIR, '..', 'datasets', 'datasets_v2_111311')
 print('[Info] data2_path: {}'.format(data2_path))
 train2_filenames, test2_filenames = get_problems_data(data2_path)
 train2_filenames = (train2_filenames + test2_filenames)
 random.shuffle(train2_filenames)
 print('[Info] data2 train: {}, test: {}'.format(len(train2_filenames), len(test2_filenames)))
 
-# data3_path = os.path.join(ROOT_DIR, '..', 'datasets', 'application_3499_1024_358w')
-# print('[Info] data3_path: {}'.format(data3_path))
-# train3_filenames, test3_filenames = get_problems_data(data3_path)
-# random.shuffle(train3_filenames)
-# train3_filenames = train3_filenames[:1000000]
-# test3_filenames = test3_filenames[:10000]
-# print('[Info] data3 train: {}, test: {}'.format(len(train3_filenames), len(test3_filenames)))
+data3_path = os.path.join(ROOT_DIR, '..', 'datasets', 'datasets_v3_187281')
+print('[Info] data3_path: {}'.format(data3_path))
+train3_filenames, test3_filenames = get_problems_data(data3_path)
+train3_filenames = (train3_filenames + test3_filenames)
+random.shuffle(train3_filenames)
+print('[Info] data3 train: {}, test: {}'.format(len(train3_filenames), len(test3_filenames)))
 
-# train_filenames = train1_filenames + train2_filenames + train3_filenames
-# test_filenames = test1_filenames + test2_filenames + test3_filenames
-
-train_filenames = train1_filenames + train2_filenames
-test_filenames = test1_filenames + test2_filenames
+train_filenames = train1_filenames + train2_filenames + train3_filenames
+test_filenames = test1_filenames + test2_filenames + test3_filenames
 
 train_filenames = train_filenames * 5
 
