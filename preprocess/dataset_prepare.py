b518e6c21ef84ecc5f8fafeccdc84a3bb721e03c
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-11-26 17:15:27 +0800

    update

--- preprocess/dataset_prepare.py
+++ preprocess/dataset_prepare.py
@@ -8,6 +8,7 @@ Created by C. L. Wang on 26.11.20
 import os
 import json
 import sys
+from multiprocessing.pool import Pool
 
 p = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if p not in sys.path:
@@ -24,6 +25,7 @@ class DatasetPrepare(object):
 
     @staticmethod
     def process_file(path, name, out_dir):
+        print('[Info] path: {}'.format(path))
         data_lines = read_file(path)
         out_file = os.path.join(out_dir, '{}_out.txt'.format(name))
 
@@ -50,8 +52,15 @@ class DatasetPrepare(object):
         paths_list, names_list = traverse_dir_files(self.file_dir)
         out_dir = os.path.join(DATA_DIR, '2020_11_26_out')
         mkdir_if_not_exist(out_dir)
+
+        pool = Pool(processes=40)
+
         for path, name in zip(paths_list, names_list):
-            DatasetPrepare.process_file(path, name, out_dir)
+            pool.apply_async(DatasetPrepare.process_file, (path, name, out_dir))
+            # DatasetPrepare.process_file(path, name, out_dir)
+
+        pool.close()
+        pool.join()
         print('[Info] 全部处理完成: {}'.format(out_dir))
 
 
