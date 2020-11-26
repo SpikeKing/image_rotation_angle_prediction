3ef163cf7b2771ac0d7152807a817e1a4a468cf7
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-11-26 17:32:11 +0800

    update

--- preprocess/dataset_prepare.py
+++ preprocess/dataset_prepare.py
@@ -10,6 +10,8 @@ import json
 import sys
 from multiprocessing.pool import Pool
 
+from x_utils.vpf_utils import get_uc_rotation_vpf_service
+
 p = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if p not in sys.path:
     sys.path.append(p)
@@ -22,9 +24,10 @@ from root_dir import ROOT_DIR, DATA_DIR
 class DatasetPrepare(object):
     def __init__(self):
         self.file_dir = os.path.join(ROOT_DIR, '..', 'datasets', '2020_11_26')
+        self.vpf_dir = os.path.join(DATA_DIR, '2020_11_26_out')
 
     @staticmethod
-    def process_file(path, name, out_dir):
+    def process_raw_file(path, name, out_dir):
         print('[Info] path: {}'.format(path))
         data_lines = read_file(path)
         out_file = os.path.join(out_dir, '{}_out.txt'.format(name))
@@ -49,7 +52,7 @@ class DatasetPrepare(object):
         write_list_to_file(out_file, out_list)
         print('[Info] 处理完成: {}'.format(out_file))
 
-    def process(self):
+    def process_raw_data(self):
         paths_list, names_list = traverse_dir_files(self.file_dir)
         out_dir = os.path.join(DATA_DIR, '2020_11_26_out')
         mkdir_if_not_exist(out_dir)
@@ -58,16 +61,60 @@ class DatasetPrepare(object):
 
         for path, name in zip(paths_list, names_list):
             # pool.apply_async(DatasetPrepare.process_file, (path, name, out_dir))
-            DatasetPrepare.process_file(path, name, out_dir)
+            DatasetPrepare.process_raw_file(path, name, out_dir)
 
         pool.close()
         pool.join()
         print('[Info] 全部处理完成: {}'.format(out_dir))
 
+    @staticmethod
+    def process_vpf_file(path, name, out_dir):
+        print('[Info] path: {}'.format(path))
+        data_lines = read_file(path)
+        name_x = name.split('.')[0]
+        out_file = os.path.join(out_dir, '{}_vpf.txt'.format(name_x))
+        create_file(out_file)
+
+        out_list = []
+        for idx, data_line in enumerate(data_lines):
+            # if idx == 100:
+            #     break
+            img_url, dmy_angle = data_line.split(',')
+            dmy_angle = int(dmy_angle)
+            try:
+                uc_dict = get_uc_rotation_vpf_service(img_url)
+                uc_angle = int(uc_dict['data']['angle'])
+            except Exception as e:
+                uc_angle = -1
+            if dmy_angle != uc_angle:
+                out_items = [img_url, str(dmy_angle), str(uc_angle)]
+                print('[Info] {} out_items: {}'.format(idx, out_items))
+            else:
+                continue
+            out_line = ",".join(out_items)
+            out_list.append(out_line)
+        write_list_to_file(out_file, out_list)
+        print('[Info] 处理完成: {}'.format(out_file))
+
+    def process_vpf_data(self):
+        paths_list, names_list = traverse_dir_files(self.vpf_dir)
+        out_dir = os.path.join(DATA_DIR, '2020_11_26_vpf')
+        mkdir_if_not_exist(out_dir)
+
+        pool = Pool(processes=40)
+
+        for path, name in zip(paths_list, names_list):
+            pool.apply_async(DatasetPrepare.process_vpf_file, (path, name, out_dir))
+            # DatasetPrepare.process_vpf_file(path, name, out_dir)
+
+        pool.close()
+        pool.join()
+
+        print('[Info] 全部处理完成: {}'.format(out_dir))
 
 def main():
     dp = DatasetPrepare()
-    dp.process()
+    dp.process_vpf_data()
 
 
 if __name__ == '__main__':
