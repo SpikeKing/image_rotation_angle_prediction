605c51e30c0a9cef0b3baf4e956500e8d549a4f6
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-11-28 10:55:02 +0800

    update

--- utils.py
+++ utils.py
@@ -259,7 +259,7 @@ def generate_rotated_image(image, angle, size=None, crop_center=False,
             image = cv2.resize(image, size)  # 普通的Resize
 
             # from myutils.cv_utils import resize_image_with_padding
-            # image = resize_image_with_padding(image, desired_size=size[0])
+            # image = resize_image_with_padding(image, desired_size=size[0]) # Padding Resize
     except Exception as e:
         image, rotated_ratio, angle = get_format_img(size)
         is_ok = False
@@ -320,7 +320,6 @@ class RotNetDataGenerator(Iterator):
             # offset_angle = random.randint(-12, 12)
 
             rotation_angle = random_pick([0, 90, 180, 270], [0.25, 0.25, 0.25, 0.25])
-
             rotation_angle = (rotation_angle + offset_angle) % 360
         else:
             rotation_angle = 0
@@ -354,17 +353,18 @@ class RotNetDataGenerator(Iterator):
                 if is_color:
                     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+                # 随机剪裁
                 # if random_prob(0.5):
                 #     h, w, _ = image.shape
                 #     if random_prob(0.5):
                 #         out_h = int(h // 2)  # mode 1
-                        # if random_prob(0.7):  # mode 2
-                        #     out_h = int(h // 2)
-                        # else:
-                        #     out_h = int(h // 3)
-                    # else:
-                    #     out_h = h
-                    # image = random_crop(image, out_h, w)
+                #         if random_prob(0.7):  # mode 2
+                #             out_h = int(h // 2)
+                #         else:
+                #             out_h = int(h // 3)
+                #     else:
+                #         out_h = h
+                #     image = random_crop(image, out_h, w)
 
             rotated_image, rotation_angle, rotated_ratio, is_ok = self.process_img(image)
             if not is_ok:
