5eedc1ef971a09fe489487342b047f5c533b4635
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-11-29 20:40:58 +0800

    update

--- train/train_problems.py
+++ train/train_problems.py
@@ -116,9 +116,9 @@ batch_size = 192
 nb_epoch = 200
 
 # 加载已有模型
-# model_path = os.path.join(DATA_DIR, 'models', 'problem_rotnet_mobilenetv2_v4_pad_20201128.3.hdf5')  # 最好模型
-# model.load_weights(model_path)
-# print('[Info] 加载模型的路径: {}'.format(model_path))
+model_path = os.path.join(DATA_DIR, 'models', 'problem_rotnet_mobilenetv2_base_20201129.1.hdf5')  # 最好模型
+model.load_weights(model_path)
+print('[Info] 加载模型的路径: {}'.format(model_path))
 
 output_folder = 'models_{}_{}'.format(len(train_filenames), get_current_time_str())
 if not os.path.exists(output_folder):
