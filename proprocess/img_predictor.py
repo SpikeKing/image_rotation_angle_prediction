cd10846816675c4bae0c620d79da1668003f75f0
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-12-01 09:43:21 +0800

    update

--- proprocess/img_predictor.py
+++ proprocess/img_predictor.py
@@ -31,7 +31,7 @@ from utils import angle_error
 
 class ImgPredictor(object):
     def __init__(self):
-        self.model_name = "problem_rotnet_mobilenetv2_20w_20201121.hdf5"
+        # self.model_name = "problem_rotnet_mobilenetv2_20w_20201121.hdf5"
         # self.model_name = "problem_rotnet_mobilenetv2_even_20201126.3.hdf5"
         # self.model_name = "problem_rotnet_mobilenetv2_123x_20201126.1.hdf5"
         # self.model_name = "problem_rotnet_mobilenetv2_a12_20201126.2.hdf5"
@@ -44,6 +44,7 @@ class ImgPredictor(object):
         # self.model_name = "problem_rotnet_mobilenetv2_base_20201130.1.hdf5"
         # self.model_name = "problem_rotnet_mobilenetv2_pad448_20201130.3.hdf5"
         # self.model_name = "problem_rotnet_mobilenetv2_basex_20201130.4.hdf5"
+        self.model_name = "problem_rotnet_mobilenetv2_pad448_base_20201201.4.hdf5"
         print('[Info] model name: {}'.format(self.model_name))
         self.model = self.load_model()
         pass
@@ -122,11 +123,11 @@ class ImgPredictor(object):
 
         img_bgr = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
 
-        img_rgb_224 = cv2.resize(img_bgr, (224, 224))  # resize
-        # img_rgb_224 = resize_image_with_padding(img_bgr, 224)  # pad
-        # img_rgb_224 = resize_image_with_padding(img_bgr, 448)  # pad
+        # img_rgb_resized = cv2.resize(img_bgr, (224, 224))  # resize
+        # img_rgb_resized = resize_image_with_padding(img_bgr, 224)  # pad
+        img_rgb_resized = resize_image_with_padding(img_bgr, 448)  # pad
 
-        img_bgr_b = np.expand_dims(img_rgb_224, axis=0)
+        img_bgr_b = np.expand_dims(img_rgb_resized, axis=0)
 
         prediction = self.model.predict([img_bgr_b, ratio_b])
         probs = prediction[0]
