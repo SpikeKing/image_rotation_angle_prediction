c5854c41533541c7033e455fec877e96e75c421b
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-12-03 16:28:34 +0800

    update

--- train/problem_trainer.py
+++ train/problem_trainer.py
@@ -98,11 +98,11 @@ class ProblemTrainer(object):
 
     def load_train_and_test_dataset(self):
         # 18w有黑边的数据集
-        dataset1_path = os.path.join(ROOT_DIR, '..', 'datasets', 'datasets_v3_187281')
-        train1_filenames = self.get_total_datasets(dataset1_path)
+        # dataset1_path = os.path.join(ROOT_DIR, '..', 'datasets', 'datasets_v3_187281')
+        # train1_filenames = self.get_total_datasets(dataset1_path)
 
         # 14w无黑边数据
-        dataset2_path = os.path.join(ROOT_DIR, '..', 'datasets', 'datasets_v4_checked')
+        dataset2_path = os.path.join(ROOT_DIR, '..', 'datasets', 'datasets_v4_checked_r')
         train2_filenames, test2_filenames = self.get_split_datasets(dataset2_path)
 
         # 1k纯粹验证集
@@ -110,7 +110,8 @@ class ProblemTrainer(object):
         test_val_filenames = self.get_total_datasets(dataset_val_path)
 
         # 全部数据集
-        train_filenames = train1_filenames + train2_filenames
+        # train_filenames = train1_filenames + train2_filenames
+        train_filenames = train2_filenames
         test_filenames = test2_filenames + test_val_filenames * 10
 
         random.shuffle(train_filenames)
