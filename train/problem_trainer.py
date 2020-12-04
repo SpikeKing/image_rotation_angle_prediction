ca621e3462354521e4f93256bb4bcd132fed98e2
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-12-04 15:16:24 +0800

    update

--- train/problem_trainer.py
+++ train/problem_trainer.py
@@ -53,12 +53,12 @@ class ProblemTrainer(object):
         self.is_random_crop_h = is_random_crop_h  # 随机高度剪裁
 
         if self.mode == "mobilenetv2":
-            self.model_path = os.path.join(DATA_DIR, 'models', 'rotnet_v3_mobilenetv2_0.01_20201204.h5')  # 最好模型
-            # self.model_path = None
+            # self.model_path = os.path.join(DATA_DIR, 'models', 'rotnet_v3_mobilenetv2_0.01_20201204.h5')  # 最好模型
+            self.model_path = None
             self.batch_size = 320  # batch size, v100
         elif self.mode == "resnet50":
-            self.model_path = os.path.join(DATA_DIR, 'models', 'rotnet_v3_resnet50_0.02_20201204.1.h5')
-            # self.model_path = None
+            # self.model_path = os.path.join(DATA_DIR, 'models', 'rotnet_v3_resnet50_0.02_20201204.1.h5')
+            self.model_path = None
             self.batch_size = 224  # batch size, v100
 
         # 输出文件夹
@@ -103,7 +103,7 @@ class ProblemTrainer(object):
             layer.trainable = False
 
         x = base_model.output
-        x = Dense(128, activation="relu")(x)
+        # x = Dense(128, activation="relu")(x)
         x = Flatten()(x)
 
         if self.is_hw_ratio:  # 是否使用宽高比
