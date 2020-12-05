4343e163c93a6c1d44152c2dd601e4f80ad8d84c
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-12-05 10:38:13 +0800

    update

--- train/problem_trainer.py
+++ train/problem_trainer.py
@@ -53,7 +53,9 @@ class ProblemTrainer(object):
         self.is_random_crop_h = is_random_crop_h  # 随机高度剪裁
 
         if self.mode == "mobilenetv2" and self.input_shape[0] == 224:
-            self.model_path = os.path.join(DATA_DIR, 'models', 'rotnet_v3_mobilenetv2_base224_20201205.1.h5')  # 最好模型
+            self.model_path = os.path.join(DATA_DIR, 'models', 'rotnet_v3_mobilenetv2_base224_20201205.1.h5')
+        elif self.mode == "mobilenetv2" and self.input_shape[0] == 448:
+            self.model_path = os.path.join(DATA_DIR, 'models', 'rotnet_v3_mobilenetv2_base448_20201205.3.h5')
         elif self.mode == "resnet50":
             self.model_path = os.path.join(DATA_DIR, 'models', 'rotnet_v3_resnet50_base448_20201205.2.h5')
 
