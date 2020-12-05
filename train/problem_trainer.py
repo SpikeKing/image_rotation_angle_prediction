69106ae535f2b29312c423002fd387573202f53a
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-12-05 10:53:36 +0800

    update

--- train/problem_trainer.py
+++ train/problem_trainer.py
@@ -37,7 +37,7 @@ class ProblemTrainer(object):
     def __init__(self,
                  mode="mobilenetv2",  # 训练模式, 支持mobilenetv2和resnet50
                  nb_classes=4,
-                 input_shape=(224, 224, 3),  # 训练模式，支持224x224x3和448x448x3
+                 input_shape=(448, 448, 3),  # 训练模式，支持224x224x3和448x448x3
                  random_angle=0,  # 随机10度
                  is_hw_ratio=False,  # 是否使用高宽比
                  nb_epoch=200,
