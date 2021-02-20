702092dfa98b8aec93fda679189169c8a9e10611
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2021-02-20 15:41:21 +0800

    update

--- train/problem_trainer.py
+++ train/problem_trainer.py
@@ -41,7 +41,7 @@ class ProblemTrainer(object):
                  random_angle=8,  # 随机10度
                  is_hw_ratio=False,  # 是否使用高宽比
                  nb_epoch=200,
-                 is_random_crop_h=True  # 随机高度剪裁
+                 is_random_crop_h=False  # 随机高度剪裁
                  ):
 
         self.mode = mode  # 训练模式
@@ -226,7 +226,7 @@ class ProblemTrainer(object):
             shuffle=True,
             is_hw_ratio=self.is_hw_ratio,
             random_angle=self.random_angle,
-            is_random_crop_h=True
+            is_random_crop_h=self.is_random_crop_h
         )
 
         test_generator = RotNetDataGenerator(
@@ -239,7 +239,7 @@ class ProblemTrainer(object):
             is_train=False,  # 关闭训练参数
             is_hw_ratio=self.is_hw_ratio,
             random_angle=self.random_angle,
-            is_random_crop_h=True
+            is_random_crop_h=self.is_random_crop_h
         )
 
         steps_per_epoch = len(self.train_data) // self.batch_size
