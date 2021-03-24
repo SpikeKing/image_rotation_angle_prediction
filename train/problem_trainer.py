9477f4968bb5733bc5bac3fd721c244f168a9ed8
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2021-03-24 10:02:52 +0800

    update

--- train/problem_trainer.py
+++ train/problem_trainer.py
@@ -41,7 +41,7 @@ class ProblemTrainer(object):
                  random_angle=10,  # 随机10度
                  is_hw_ratio=False,  # 是否使用高宽比
                  nb_epoch=200,
-                 is_random_crop=True  # 随机高度剪裁
+                 is_random_crop=False  # 随机高度剪裁
                  ):
 
         self.mode = mode  # 训练模式
