986bff7a597b59d62bd7204d3ce6d45e6d383e30
morndragon <morndragon@126.com>
2021-09-06 20:58:03 +0800

    update

--- proprocess_v2/dataset_error_checker.py
+++ proprocess_v2/dataset_error_checker.py
@@ -15,12 +15,16 @@ if p not in sys.path:
     sys.path.append(p)
 
 from root_dir import ROOT_DIR, DATA_DIR
+from x_utils.vpf_sevices import get_vpf_service_np
 from myutils.project_utils import *
+from myutils.cv_utils import *
 
 
 class DatasetErrorChecker(object):
     def __init__(self):
         self.out_img_paths_file = os.path.join(DATA_DIR, "files_v2", "dataset_all_path_{}.txt".format(get_current_time_str()))
+        self.out_img_paths_file_x = os.path.join(DATA_DIR, "files_v2", "dataset_all_path_20210906.txt")
+        self.out_error_paths_file = os.path.join(DATA_DIR, "files_v2", "dataset_all_path_20210906_error_{}.txt".format(get_current_time_str()))
 
     @staticmethod
     def process_line(folder_path, out_file):
@@ -58,13 +62,47 @@ class DatasetErrorChecker(object):
         pool.join()
         print('[Info] 写入完成: {}'.format(self.out_img_paths_file))
 
+    @staticmethod
+    def save_img_path(img_bgr, img_name, oss_root_dir=""):
+        """
+        上传图像
+        """
+        from x_utils.oss_utils import save_img_2_oss
+        if not oss_root_dir:
+            oss_root_dir = "zhengsheng.wcl/Image-Rotation/imgs-tmp/{}".format(get_current_day_str())
+        img_url = save_img_2_oss(img_bgr, img_name, oss_root_dir)
+        return img_url
+
+    @staticmethod
+    def process_img_path(img_idx, img_path, service,  out_file):
+        img_bgr = cv2.imread(img_path)
+        res_dict = get_vpf_service_np(img_np=img_bgr, service_name=service)  # 表格
+        angle = res_dict["data"]["angle"]
+        angle = int(angle)
+        if angle != 0:
+            items = img_path.split("/")
+            img_name = "{}-f-{}.jpg".format(items[-2], items[-1])
+            img_url = DatasetErrorChecker.save_img_path(img_bgr, img_name)
+            write_line(out_file, "{}\t{}".format(img_url, angle))
+            print('[Info] 处理完成: {}, angle: {}'.format(img_idx, angle))
+
     def check(self):
-        pass
+        data_lines = read_file(self.out_img_paths_file_x)
+        print('[Info] 样本数: {}'.format(len(data_lines)))
+        service = "eR8qKVKScPRgWWfJQCDmXo"
+        pool = Pool(processes=100)
+        for data_idx, data_line in enumerate(data_lines):
+            pool.apply_async(DatasetErrorChecker.process_img_path,
+                             (data_idx, data_line, service, self.out_error_paths_file))
+        pool.close()
+        pool.join()
+        data_lines = read_file(self.out_error_paths_file)
+        print('[Info] 处理完成: {}, 样本数: {}'.format(self.out_error_paths_file, len(data_lines)))
 
 
 def main():
     dc = DatasetErrorChecker()
-    dc.load_dataset()
+    dc.check()
 
 
 if __name__ == '__main__':
