4eced4496d2d34fbeed9a606e66fd1aa7721c0a2
morndragon <morndragon@126.com>
2021-09-07 16:19:18 +0800

    update

--- proprocess_v2/dataset_urls_checker.py
+++ proprocess_v2/dataset_urls_checker.py
@@ -7,6 +7,7 @@ Created by C. L. Wang on 7.9.21
 
 import os
 import sys
+from multiprocessing.pool import Pool
 
 p = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if p not in sys.path:
@@ -22,6 +23,7 @@ from x_utils.vpf_sevices import get_vpf_service
 class DatasetUrlsChecker(object):
     def __init__(self):
         self.file_path = os.path.join(DATA_DIR, "files_v2", "k12_urls_english.txt")
+        self.file_txt_path = os.path.join(DATA_DIR, "files_v2", "k12_urls_english.check.txt")
         self.file_html_path = os.path.join(DATA_DIR, "files_v2", "k12_urls_english.check.html")
 
     @staticmethod
@@ -42,6 +44,21 @@ class DatasetUrlsChecker(object):
         img_url = save_img_2_oss(img_bgr, img_name, oss_root_dir)
         return img_url
 
+    @staticmethod
+    def process_item(data_idx, data_line, out_path):
+        angle = DatasetUrlsChecker.call_service(data_line)
+        angle = int(angle)
+        _, img_bgr = download_url_img(data_line)
+        if angle != 0:
+            print('[Info] angle: {}'.format(angle))
+            print('[Info] data_line: {}'.format(data_line))
+            img_bgr = rotate_img_for_4angle(img_bgr, angle)
+
+        img_name = "{}-{}.jpg".format(data_idx, get_current_time_str())
+        img_url = DatasetUrlsChecker.save_img_path(img_bgr, img_name)
+        write_line(out_path, img_url)
+        print('[Info] data_idx: {}'.format(data_idx))
+
     def process(self):
         data_lines = read_file(self.file_path)
         print('[Info] 处理文件: {}'.format(self.file_path))
@@ -53,20 +70,15 @@ class DatasetUrlsChecker(object):
         data_lines = data_lines[:200]
         print('[Info] 样本数: {}'.format(len(data_lines)))
 
-        urls_list = []
+        pool = Pool(processes=100)
         for data_idx, data_line in enumerate(data_lines):
-            print('[Info] data_line: {}'.format(data_line))
-            angle = DatasetUrlsChecker.call_service(data_line)
-            angle = int(angle)
-            print('[Info] angle: {}'.format(angle))
-            if angle != 0:
-                _, img_bgr = download_url_img(data_line)
-                img_bgr = rotate_img_for_4angle(img_bgr, angle)
-                img_name = "{}-{}.jpg".format(data_idx, get_current_time_str())
-                img_url = DatasetUrlsChecker.save_img_path(img_bgr, img_name)
-                urls_list.append([img_url])
-            else:
-                urls_list.append([data_line])
+            DatasetUrlsChecker.process_item(data_idx, data_line, self.file_txt_path)
+            # pool.apply_async(DatasetUrlsChecker.process_item, (data_idx, data_line, self.file_txt_path))
+        pool.close()
+        pool.join()
+
+        urls_list = read_file(self.file_txt_path)
+        urls_list = [[x] for x in urls_list]
 
         make_html_page(self.file_html_path, urls_list)
         print('[Info] 处理完成: {}'.format(self.file_html_path))
