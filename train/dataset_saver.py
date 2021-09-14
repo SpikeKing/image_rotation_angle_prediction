f113a90065a06ef405a3a6ce0a1efd3acb8cf7bf
morndragon <morndragon@126.com>
2021-09-14 17:52:35 +0800

    update

--- train/dataset_saver.py
+++ train/dataset_saver.py
@@ -7,13 +7,14 @@ Created by C. L. Wang on 14.9.21
 
 import os
 import sys
+from multiprocessing import Pool
 
 p = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if p not in sys.path:
     sys.path.append(p)
 
 from myutils.project_utils import *
-from root_dir import DATA_DIR
+from root_dir import DATA_DIR, ROOT_DIR
 from train.problem_trainer import ProblemTrainer
 
 
@@ -22,30 +23,60 @@ class DatasetSaver(object):
     预存数据文件，避免遍历速度过慢
     """
     def __init__(self):
-        self.train_format_file = os.path.join(DATA_DIR, "files_v2", "train_dataset_all_{}.txt")
-        self.test_format_file = os.path.join(DATA_DIR, "files_v2", "test_dataset_all_{}.txt")
+        self.data_file = os.path.join(DATA_DIR, "files_v2", "angle_dataset_all_{}.txt".format(get_current_day_str()))
 
-    def process(self):
-        train_filenames, test_filenames = ProblemTrainer.load_train_and_test_dataset_v1()
-        print('[Info] 训练样本数: {}'.format(len(train_filenames)))
-        print('[Info] 测试样本数: {}'.format(len(test_filenames)))
-        train_path = self.train_format_file.format(len(train_filenames))
-        test_path = self.test_format_file.format(len(test_filenames))
-        if os.path.exists(train_path):
-            print("[Info] 文件存在: {}".format(train_path))
-            return
-        if os.path.exists(test_path):
-            print("[Info] 文件存在: {}".format(test_path))
-            return
-        write_list_to_file(train_path, train_filenames)
-        write_list_to_file(test_path, test_filenames)
-        print('[Info] 写入完成: {}'.format(train_path))
-        print('[Info] 写入完成: {}'.format(test_path))
+    @staticmethod
+    def process_line(folder_path, out_file):
+        print('[Info] 读取路径: {}'.format(folder_path))
+        paths_list, names_list = traverse_dir_files(folder_path)
+        print('[Info] 读取完成: {}'.format(len(paths_list)))
+        write_list_to_file(out_file, paths_list)
+        print('[Info] 写入完成: {}, 样本数: {}'.format(folder_path, len(paths_list)))
+
+    def load_dataset(self):
+        # 自然场景图像, 71885
+        dataset13_path = os.path.join(ROOT_DIR, '..', 'datasets', 'rotation_datasets_nat_v2_raw_20210829_1024')
+        # 自然场景图像, 13315
+        dataset12_path = os.path.join(ROOT_DIR, '..', 'datasets', 'rotation_datasets_nat_20210828')
+        # 表格图像, 42579
+        dataset11_path = os.path.join(ROOT_DIR, '..', 'datasets', 'rotation_datasets_table_20210828')
+        # 图像翻译图像, 88986
+        dataset10_path = os.path.join(ROOT_DIR, '..', 'datasets', 'rotation_datasets_trans_20210828')
+        # 其他图像, 2060
+        dataset9_path = os.path.join(ROOT_DIR, '..', 'datasets', 'rotation_ds_other_1024')
+        # 22w小图数据集
+        dataset8_path = os.path.join(ROOT_DIR, '..', 'datasets', 'rotation_ds_xiaotu_25w')
+        # 14w query数据, 已验证
+        dataset1_path = os.path.join(ROOT_DIR, '..', 'datasets', 'segmentation_ds_v4', 'images')
+        # 12w query数据
+        dataset2_path = os.path.join(ROOT_DIR, '..', 'datasets', 'datasets_v4_checked_r')
+        # 5k 题库数据, 4714
+        dataset3_path = os.path.join(ROOT_DIR, '..', 'datasets', 'rotation_ds_tiku_5k')
+        # 2w 题库数据, 21248
+        dataset4_path = os.path.join(ROOT_DIR, '..', 'datasets', 'rotation_ds_page_2w')
+        # 4w 手写数据, 37548
+        dataset5_path = os.path.join(ROOT_DIR, '..', 'datasets', 'rotation_ds_write_4w')
+        # 3w 手写数据, 28429
+        dataset6_path = os.path.join(ROOT_DIR, '..', 'datasets', 'rotation_ds_write2_3w')
+        # 2.2w 题库修改数据, 21169
+        dataset7_path = os.path.join(ROOT_DIR, '..', 'datasets', 'rotation_ds_page_bkg_2w')
+
+        dataset_num_list = \
+            [[dataset13_path, -1], [dataset12_path, -1], [dataset11_path, -1], [dataset10_path, -1], [dataset9_path, -1],
+             [dataset8_path, 100000], [dataset1_path, 100000], [dataset2_path, 100000], [dataset3_path, -1],
+             [dataset4_path, -1], [dataset5_path, -1], [dataset6_path, -1], [dataset7_path, -1]]
+
+        pool = Pool(processes=100)
+        for folder_path, num in dataset_num_list:
+            pool.apply_async(DatasetSaver.process_line, (folder_path, num, self.data_file))
+        pool.close()
+        pool.join()
+        print('[Info] 写入完成: {}'.format(self.data_file))
 
 
 def main():
     ds = DatasetSaver()
-    ds.process()
+    ds.load_dataset()
 
 
 if __name__ == '__main__':
