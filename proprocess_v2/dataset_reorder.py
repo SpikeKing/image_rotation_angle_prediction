abf14cea3224ef5faf52b544bebf332834dcbe03
morndragon <morndragon@126.com>
2021-10-25 14:28:23 +0800

    update

--- proprocess_v2/dataset_reorder.py
+++ proprocess_v2/dataset_reorder.py
@@ -32,10 +32,17 @@ class DatasetReorder(object):
         self.out_files_folder = os.path.join(DATA_DIR, "files_v2", "angle_dataset_all_20211025")
         self.out_ds_folder = os.path.join(ROOT_DIR, "..", "datasets", "angle_datasets")
 
+    @staticmethod
+    def copy_line_mul(data_idx, data_line, type_name, dataset_folder, out_path_file):
+        data_idx_str = str(data_idx).zfill(6)
+        out_name = "{}_{}.jpg".format(type_name, data_idx_str)
+        out_path = os.path.join(dataset_folder, out_name)
+        shutil.copy(data_line, out_path)
+        write_line(out_path_file, out_path)
+        if data_idx % 100 == 0:
+            print('[Info] \t{}'.format(data_idx))
+
     def merge_hardcase(self, file_list, type_name):
-        # file1 = os.path.join(self.folder, "rotation_datasets_hardcase.112.txt")
-        # file2 = os.path.join(self.folder, "rotation_ds_other_1024_20210927.txt")
-        # type_name = "hardcase"
         data_lines = []
         for file_name in file_list:
             file_path = os.path.join(self.folder, file_name)
@@ -45,21 +52,20 @@ class DatasetReorder(object):
 
         folder_name = "dataset_{}_{}".format(type_name, len(data_lines))
         dataset_folder = os.path.join(self.out_ds_folder, folder_name)
+        mkdir_if_not_exist(dataset_folder)
         print('[Info] 输出文件夹路径: {}'.format(dataset_folder))
+
+        mkdir_if_not_exist(self.out_files_folder)
         out_path_file = os.path.join(self.out_files_folder, "{}.txt".format(folder_name))
         print('[Info] 输出文件路径: {}'.format(out_path_file))
 
-        mkdir_if_not_exist(dataset_folder)
-        path_list = []
+        pool = Pool(processes=100)
         for data_idx, data_line in enumerate(data_lines):
-            data_idx_str = str(data_idx).zfill(6)
-            out_name = "{}_{}.jpg".format(type_name, data_idx_str)
-            out_path = os.path.join(dataset_folder, out_name)
-            shutil.copy(data_line, out_path)
-            path_list.append(out_path)
-            if data_idx % 5000 == 0:
-                print('[Info] \t{}'.format(data_idx))
-        write_list_to_file(out_path_file, path_list)
+            pool.apply_async(
+                DatasetReorder.copy_line_mul, (data_idx, data_line, type_name, dataset_folder, out_path_file))
+        pool.close()
+        pool.join()
+        path_list = read_file(out_path_file)
         print('[Info] 输出路径: {}, 样本数: {}'.format(len(path_list), len(data_lines)))
         print('[Info] 处理完成: {}'.format(out_path_file))
 
