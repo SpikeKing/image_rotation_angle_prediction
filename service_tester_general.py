ff0280ba7e2c3fa74d66943d630f87efa305cfdd
morndragon <morndragon@126.com>
2021-10-26 14:24:05 +0800

    update

--- service_tester_general.py
+++ service_tester_general.py
@@ -14,7 +14,10 @@ from myutils.project_utils import *
 from x_utils.vpf_sevices import get_vpf_service_np
 
 
-class ServiceTester(object):
+class ServiceTesterGeneral(object):
+    """
+    通用的服务测试逻辑
+    """
     def __init__(self, in_folder, service, out_folder, max_num):
         self.in_folder = in_folder
         self.out_folder = out_folder
@@ -23,7 +26,7 @@ class ServiceTester(object):
         print('[Info] 输入文件夹: {}'.format(self.in_folder))
         print('[Info] 服务: {}'.format(self.service))
         print('[Info] 输出文件夹: {}'.format(self.out_folder))
-        print('[Info] 最大测试量: {}'.format(self.max_num))
+        print('[Info] 最大样本数: {}'.format(self.max_num))
 
     @staticmethod
     def save_img_path(img_bgr, img_name, oss_root_dir=""):
@@ -44,42 +47,53 @@ class ServiceTester(object):
         angle = int(angle)
         if angle != 0:
             img_name = img_path.split("/")[-1]
-            img_url = ServiceTester.save_img_path(img_bgr, img_name)
+            img_url = ServiceTesterGeneral.save_img_path(img_bgr, img_name)
             write_line(out_file, "{}\t{}\t{}".format(img_url, angle, img_path))
         if img_idx % 1000 == 0:
             print('[Info] 处理完成: {}, angle: {}'.format(img_idx, angle))
 
     def process_folder(self):
-        if self.in_folder.endswith(".txt"):
-            paths_list = read_file(self.in_folder)
-        else:
-            paths_list, _ = traverse_dir_files(self.in_folder)
-        print('[Info] 样本数: {}'.format(len(paths_list)))
-        random.seed(47)
-        random.shuffle(paths_list)
-        if len(paths_list) > self.max_num:
-            paths_list = paths_list[:self.max_num]
-        print('[Info] 样本数: {}'.format(len(paths_list)))
-        time_str = get_current_time_str()
-        out_file = os.path.join(self.out_folder, "val_{}.txt".format(time_str))
-        out_html = os.path.join(self.out_folder, "val_{}.html".format(time_str))
+        print('[Info] 测试文件夹: {}'.format(self.in_folder))
+        file_paths, file_names = traverse_dir_files(self.in_folder)
+        data_dict = dict()
+        for file_path, file_name in zip(file_paths, file_names):
+            data_lines = read_file(file_path)
+            data_dict[file_name] = data_lines
+
+        out_dict = dict()
         pool = Pool(processes=100)
-        for img_idx, img_path in enumerate(paths_list):
-            if "rotation_datasets_hardcase" in img_path:  # 过滤hardcase
-                continue
-            # ServiceTester.process_img_path(img_idx, img_path, self.service, out_file)
-            pool.apply_async(ServiceTester.process_img_path, (img_idx, img_path, self.service, out_file))
+        for file_name in data_dict.keys():
+            data_lines = data_dict[file_name]
+            random.seed(47)
+            random.shuffle(data_lines)
+            if len(data_lines) > self.max_num:
+                data_lines = data_lines[:self.max_num]
+            print('[Info] 文件名: {}, 样本数: {}'.format(file_name, len(data_lines)))
+
+            time_str = get_current_time_str()
+            type_name = file_name.split(".")[0]
+            sub_folder = os.path.join(self.out_folder, type_name)
+            out_file = os.path.join(sub_folder, "out_{}.txt".format(time_str))
+            out_html = os.path.join(sub_folder, "out_{}.html".format(time_str))
+            out_dict[file_name] = [out_file, out_html]
+            for img_idx, img_path in enumerate(data_lines):
+                pool.apply_async(ServiceTesterGeneral.process_img_path, (img_idx, img_path, self.service, out_file))
         pool.close()
         pool.join()
-        print('[Info] 处理完成: {}'.format(out_file))
-        data_lines = read_file(out_file)
-        print('[Info] 正确率: {}'.format(safe_div(len(paths_list) - len(data_lines), len(paths_list))))
-        out_list = []
-        for data_line in data_lines:
-            items = data_line.split("\t")
-            out_list.append(items)
-        make_html_page(out_html, out_list)
-        print('[Info] 处理完成: {}'.format(out_html))
+        print('[Info] 服务处理完成!')
+        print('[Info] 最终结果: ')
+        for file_name in out_dict.keys():
+            out_file, out_html = out_dict[file_name]
+            data_lines = data_dict[file_name]
+            out_lines = read_file(out_file)
+            right_rate = safe_div(len(data_lines) - len(out_lines), len(data_lines))
+            print('[Info] \t文件: {}, 正确率: {}%'.format(file_name, right_rate * 100))
+            out_list = []
+            for data_line in data_lines:
+                items = data_line.split("\t")
+                out_list.append(items)
+            make_html_page(out_html, out_list)
+        print('[Info] 全部处理完成')
 
 
 def parse_args():
@@ -97,7 +111,6 @@ def parse_args():
     arg_in_folder = args.in_folder
     print("测试文件夹: {}".format(arg_in_folder))
 
-    # ysu362VFeRZkizhfBkfbck
     arg_service = args.service
     print("服务: {}".format(arg_service))
 
@@ -113,7 +126,7 @@ def parse_args():
 
 def main():
     arg_in_folder, arg_service, arg_out_folder, arg_max_num = parse_args()
-    st = ServiceTester(arg_in_folder, arg_service, arg_out_folder, arg_max_num)
+    st = ServiceTesterGeneral(arg_in_folder, arg_service, arg_out_folder, arg_max_num)
     st.process_folder()
 
 
